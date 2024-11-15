4fe0bd13f090694bb82157e8a4301d134c6a1294
Khanh962003 <phangiakhanh962003@gmail.com>
2024-11-15 16:05:29 +0700

    Tạo menu và các chế độ chơi game

--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@ void TaoMoi(int& x_moi, int& y_moi, CONRAN& r);
 void xoacontro();
 void VeTuong();
 void NhapNhay(CONRAN& r);
+void MenuBatDauChoi();
+void CheDoCoDien(int& level);
+void ChonCheDoChoi();
+void ChonMucDoChoi(int& level);
 bool check_ran_de_moi(CONRAN& r, int x_moi, int y_moi);
 
 struct Point {
@@ -21,9 +25,11 @@ class CONRAN {
 public:
     struct Point A[100];
     int DoDai;
+    int TocDoRan;
 
     CONRAN() {
         DoDai = 3;
+        TocDoRan = 0;
         A[0].x = 8; A[0].y = 10;
         A[1].x = 7; A[1].y = 10;
         A[2].x = 6; A[2].y = 10;
@@ -55,7 +61,8 @@ public:
         // Kiểm tra nếu con rắn ăn mồi
         if (A[0].x == x_moi && A[0].y == y_moi) {
             DoDai++;  // Tăng độ dài con rắn
-            TaoMoi(x_moi, y_moi, r);  // Gọi hàm TaoMoi để tạo mồi mới
+            TaoMoi(x_moi, y_moi, r); // Gọi hàm TaoMoi để tạo mồi mới
+            TocDoRan--; //Mỗi khi tăng chiều dài tốc độ rắn tăng dần
         }
 
         //Kiểm tra việc đụng tường
@@ -64,7 +71,7 @@ public:
             system("cls");
             cout << "Game Over!" << endl;
             Sleep(2000);
-            exit(0);
+            MenuBatDauChoi();
         }
 
         //Kiểm tra việc cắn trúng thân
@@ -74,18 +81,86 @@ public:
                 system("cls");
                 cout << "Game Over!" << endl;
                 Sleep(2000);
-                exit(0);
+                MenuBatDauChoi();
             }
         }
     }
 };
 
 int main() {
+    MenuBatDauChoi();
+    return 0;
+}
+
+void MenuBatDauChoi() {
+    int choose = 0;
+    cout << "WECOME TO SNAKE GAME" << endl;
+    cout << "-----------------------------------------------------------" << endl;
+    cout << "1. Start" << endl;
+    cout << "2. Quit" << endl;
+    cout << "You choose: ";
+    cin >> choose;
+    if (choose == 1) {
+        system("cls");
+        ChonCheDoChoi();
+    }
+    else {
+        cout << "Good bye!!!" << endl;
+        exit(0);
+    }
+}
+
+void ChonCheDoChoi()
+{
+    int choose = 0, level = 0;
+    cout << "PLEASE CHOOSE GAME MODE" << endl;
+    cout << "-----------------------------------------------------------" << endl;
+    cout << "1. Classic Mode" << endl;
+    cout << "2. Free Mode" << endl;
+    cout << "3. Back" << endl;
+    cout << "You choose: ";
+    cin >> choose;
+    if (choose == 1) {
+        system("cls");
+        ChonMucDoChoi(level);
+        CheDoCoDien(level);
+    }
+    else if (choose == 1) {
+        system("cls");
+        //CheDoCoDien();
+    }
+    else {
+        system("cls");
+        MenuBatDauChoi();
+    }
+}
+
+void ChonMucDoChoi(int& level)
+{
+    cout << "PLEASE CHOOSE GAME LEVEL" << endl;
+    cout << "-----------------------------------------------------------" << endl;
+    cout << "1. Easy" << endl;
+    cout << "2. Medium" << endl;
+    cout << "3. Hard" << endl;
+    cout << "4. Back" << endl;
+    cout << "You choose: ";
+    cin >> level;
+    system("cls");
+    if (level == 4) ChonCheDoChoi();
+}
+
+void CheDoCoDien(int& level)
+{
     xoacontro();
     srand(time(0)); // Khởi tạo seed cho hàm rand
     CONRAN r;
     int Huong = 0, x_moi = 0, y_moi = 0;
     char t;
+
+    //Tạo tốc độ rắn theo từng mức độ
+    if (level == 1) r.TocDoRan = 200;
+    else if (level == 2) r.TocDoRan = 100;
+    else if (level == 3) r.TocDoRan = 50;
 
     // Tạo mồi ban đầu
     TaoMoi(x_moi, y_moi, r);
@@ -117,11 +192,9 @@ int main() {
         r.Ve();
 
         // Tốc độ di chuyển của con rắn
-        Sleep(100);
+        Sleep(r.TocDoRan);
     }
-    return 0;
 }
-
 void NhapNhay(CONRAN& r) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < r.DoDai; j++) {
@@ -133,6 +206,7 @@ void NhapNhay(CONRAN& r) {
         Sleep(500);
     }
 }
+
 void TaoMoi(int& x_moi, int& y_moi, CONRAN& r) {
     do {
         // Tạo tọa độ ngẫu nhiên cho mồi
