2a726113c385361ee092b9d2e001f9531bc680a5
UIT-21522010 <116903819+Tr4nDuy@users.noreply.github.com>
2024-12-31 15:11:04 +0700

    đồng bộ tên sang tiếng anh

--- main.cpp
+++ main.cpp
@@ -12,82 +12,82 @@ using namespace std;
 // Forward declaration
 struct Point;
 struct HighScoreEntry;
-class CONRAN;
+class SNAKE;
 
 void GoToXY(int column, int line);
-void TaoMoi(int& x_moi, int& y_moi, CONRAN& r);
-void XoaConTro();
-void VeTuong(int gameMode);
-void NhapNhay(CONRAN& r);
-void MenuBatDauChoi();
-void CheDoCoDien(int& level, int& score, vector<HighScoreEntry>& highscore);
-void CheDoTuDo(int& level, int& score, vector<HighScoreEntry>& highscore);
-void ChonCheDoChoi();
-void ChonMucDoChoi(int& level);
-bool CheckRanDeMoi(CONRAN& r, int x_moi, int y_moi);
-void DocDiemCao(vector<HighScoreEntry>& highScores);
-void VietDiemCao(vector<HighScoreEntry>& highScores);
-void ThemDiemCao(int score);
-void HienThiDiemCao(int x, int y);
-void AmThanhAnMoi();
-void AmThanhThuaCuoc();
-void DemNguocBatDau();
+void GenerateFood(int& x_food, int& y_food, SNAKE& r);
+void HideCursor();
+void DrawBoundary(int gameMode);
+void Blink(SNAKE& r);
+void StartMenu();
+void ClassicMode(int& level, int& score, vector<HighScoreEntry>& highscore);
+void FreeMode(int& level, int& score, vector<HighScoreEntry>& highscore);
+void SelectGameMode();
+void SelectDifficultyLevel(int& level);
+bool CheckFoodCollision(SNAKE& r, int x_food, int y_food);
+void ReadHighScores(vector<HighScoreEntry>& highScores);
+void WriteHighScores(vector<HighScoreEntry>& highScores);
+void AddHighScore(int score);
+void DisplayHighScore(int x, int y);
+void PlayFoodSound();
+void PlayGameOverSound();
+void Countdown();
 
 struct Point {
     int x, y;
 };
 
-class CONRAN {
+class SNAKE {
 public:
-    struct Point A[100];
-    int DoDai;
-    int TocDoRan;
+    struct Point body[100];
+    int length;
+    int speed;
 
-    CONRAN() {
-        DoDai = 3;
-        TocDoRan = 0;
-        A[0].x = 8; A[0].y = 10;
-        A[1].x = 7; A[1].y = 10;
-        A[2].x = 6; A[2].y = 10;
+    SNAKE() {
+        length = 3;
+        speed = 0;
+        body[0].x = 8; body[0].y = 10;
+        body[1].x = 7; body[1].y = 10;
+        body[2].x = 6; body[2].y = 10;
     }
 
     void Ve() {
         // Vẽ con rắn ở các vị trí mới
-        GoToXY(A[0].x, A[0].y);
+        GoToXY(body[0].x, body[0].y);
         cout << "O";
-        for (int i = 1; i < DoDai; i++) {
-            GoToXY(A[i].x, A[i].y);
-            if (A[0].x == A[i].x && A[0].y == A[i].y) cout << "O";
+        for (int i = 1; i < length; i++) {
+            GoToXY(body[i].x, body[i].y);
+            if (body[0].x == body[i].x && body[0].y == body[i].y) cout << "O";
             else cout << "-";  // In ký tự "-" để vẽ con rắn
         }
     }
 
-    void DiChuyen(int direction, int& x_moi, int& y_moi, CONRAN& r, int gameMode, int level, int& score) {
+    void DiChuyen(int direction, int& x_food, int& y_food, SNAKE& r, int gameMode, int level, int& score) {
         // Di chuyển con rắn và kiểm tra ăn mồi
-        for (int i = DoDai - 1; i > 0; i--) {
-            A[i] = A[i - 1];
+        for (int i = length - 1; i > 0; i--) {
+            body[i] = body[i - 1];
         }
 
         // Cập nhật vị trí đầu con rắn theo hướng
-        if (direction == 0) A[0].x = A[0].x + 1; // Phải
-        if (direction == 1) A[0].y = A[0].y + 1; // Xuống
-        if (direction == 2) A[0].x = A[0].x - 1; // Trái
-        if (direction == 3) A[0].y = A[0].y - 1; // Lên
+        if (direction == 0) body[0].x = body[0].x + 1; // Phải
+        if (direction == 1) body[0].y = body[0].y + 1; // Xuống
+        if (direction == 2) body[0].x = body[0].x - 1; // Trái
+        if (direction == 3) body[0].y = body[0].y - 1; // Lên
 
         if (gameMode == 2) {
-            if (A[0].x > 100) A[0].x = 0;
-            else if (A[0].x < 0) A[0].x = 100;
-            if (A[0].y > 25) A[0].y = 0;
-            else if (A[0].y < 0) A[0].y = 25;
-            VeTuong(2);
+            if (body[0].x > 100) body[0].x = 0;
+            else if (body[0].x < 0) body[0].x = 100;
+            if (body[0].y > 25) body[0].y = 0;
+            else if (body[0].y < 0) body[0].y = 25;
+            DrawBoundary(2);
         }
 
         // Kiểm tra nếu con rắn ăn mồi
-        if (A[0].x == x_moi && A[0].y == y_moi) {
-            AmThanhAnMoi();
-            DoDai++;  // Tăng độ dài con rắn
-            TaoMoi(x_moi, y_moi, r); // Gọi hàm TaoMoi để tạo mồi mới
-            TocDoRan--; //Mỗi khi tăng chiều dài tốc độ rắn tăng dần
+        if (body[0].x == x_food && body[0].y == y_food) {
+            PlayFoodSound();
+            length++;  // Tăng độ dài con rắn
+            GenerateFood(x_food, y_food, r); // Gọi hàm GenerateFood để tạo mồi mới
+            speed--; //Mỗi khi tăng chiều dài tốc độ rắn tăng dần
             //Tính điểm
             if (gameMode == 1) {
                 if (level == 1) score += 2;
@@ -103,27 +103,27 @@ public:
 
         // Kiểm tra việc đụng tường
         if (gameMode == 1) {
-            if (A[0].x == 0 || A[0].x == 100 || A[0].y == 0 || A[0].y == 25) {
-                AmThanhThuaCuoc();
-                NhapNhay(r);
+            if (body[0].x == 0 || body[0].x == 100 || body[0].y == 0 || body[0].y == 25) {
+                PlayGameOverSound();
+                Blink(r);
                 system("cls");
                 cout << "Game Over!" << endl;
                 Sleep(2000);
-                ThemDiemCao(score);  // Cập nhật điểm cao nếu cần
-                MenuBatDauChoi();
+                AddHighScore(score);  // Cập nhật điểm cao nếu cần
+                StartMenu();
             }
         }
 
         // Kiểm tra việc cắn trúng thân
-        for (int i = DoDai - 1; i > 0; i--) {
-            if (A[0].x == A[i].x && A[0].y == A[i].y) {
-                AmThanhThuaCuoc();
-                NhapNhay(r);
+        for (int i = length - 1; i > 0; i--) {
+            if (body[0].x == body[i].x && body[0].y == body[i].y) {
+                PlayGameOverSound();
+                Blink(r);
                 system("cls");
                 cout << "Game Over!" << endl;
                 Sleep(2000);
-                ThemDiemCao(score);  // Cập nhật điểm cao nếu cần
-                MenuBatDauChoi();
+                AddHighScore(score);  // Cập nhật điểm cao nếu cần
+                StartMenu();
             }
         }
     }
@@ -135,12 +135,12 @@ struct HighScoreEntry {
 };
 
 int main() {
-    MenuBatDauChoi();
+    StartMenu();
     Beep(1000, 200);
     return 0;
 }
 
-void MenuBatDauChoi() {
+void StartMenu() {
     int choose = 0;
     cout << "WELCOME TO SNAKE GAME" << endl;
     cout << "-----------------------------------------------------------" << endl;
@@ -150,7 +150,7 @@ void MenuBatDauChoi() {
     cin >> choose;
     if (choose == 1) {
         system("cls");
-        ChonCheDoChoi();
+        SelectGameMode();
     }
     else {
         cout << "Good bye!!!" << endl;
@@ -158,10 +158,10 @@ void MenuBatDauChoi() {
     }
 }
 
-void ChonCheDoChoi() {
+void SelectGameMode() {
     int choose = 0, level = 0, score = 0;
     vector<HighScoreEntry> highscore;
-    DocDiemCao(highscore);  // Đọc điểm cao từ file
+    ReadHighScores(highscore);  // Đọc điểm cao từ file
     cout << "PLEASE CHOOSE GAME Mode" << endl;
     cout << "-----------------------------------------------------------" << endl;
     cout << "1. Classic Mode" << endl;
@@ -173,34 +173,34 @@ void ChonCheDoChoi() {
     try {
         if (choose == 1) {
             system("cls");
-            ChonMucDoChoi(level);
-            CheDoCoDien(level, score, highscore);
+            SelectDifficultyLevel(level);
+            ClassicMode(level, score, highscore);
         }
         else if (choose == 2) {
             system("cls");
-            ChonMucDoChoi(level);
-            CheDoTuDo(level, score, highscore);
+            SelectDifficultyLevel(level);
+            FreeMode(level, score, highscore);
         }
         else {
             system("cls");
-            MenuBatDauChoi();
+            StartMenu();
         }
     } catch (const exception& e) {
         system("cls");
         cout << "An error occurred: " << e.what() << endl;
         cout << "Returning to main menu..." << endl;
         Sleep(2000); // Đợi 2 giây để người dùng đọc lỗi
-        MenuBatDauChoi();
+        StartMenu();
     } catch (...) {
         system("cls");
         cout << "An unknown error occurred." << endl;
         cout << "Returning to main menu..." << endl;
         Sleep(2000); // Đợi 2 giây để người dùng đọc lỗi
-        MenuBatDauChoi();
+        StartMenu();
     }
 }
 
-void ChonMucDoChoi(int& level) {
+void SelectDifficultyLevel(int& level) {
     cout << "PLEASE CHOOSE GAME level" << endl;
     cout << "-----------------------------------------------------------" << endl;
     cout << "1. Easy" << endl;
@@ -210,26 +210,26 @@ void ChonMucDoChoi(int& level) {
     cout << "You choose: ";
     cin >> level;
     system("cls");
-    if (level == 4) ChonCheDoChoi();
+    if (level == 4) SelectGameMode();
 }
 
-void CheDoCoDien(int& level, int& score, vector<HighScoreEntry>& highscore) {
-    DemNguocBatDau();
-    XoaConTro();
+void ClassicMode(int& level, int& score, vector<HighScoreEntry>& highscore) {
+    Countdown();
+    HideCursor();
     srand(time(0)); // Khởi tạo seed cho hàm rand
-    CONRAN r;
-    int direction = 0, x_moi = 0, y_moi = 0;
+    SNAKE r;
+    int direction = 0, x_food = 0, y_food = 0;
     char t;
 
     // Tạo tốc độ rắn theo từng mức độ
-    if (level == 1) r.TocDoRan = 200;
-    else if (level == 2) r.TocDoRan = 100;
-    else if (level == 3) r.TocDoRan = 50;
+    if (level == 1) r.speed = 200;
+    else if (level == 2) r.speed = 100;
+    else if (level == 3) r.speed = 50;
 
     // Tạo mồi ban đầu
-    TaoMoi(x_moi, y_moi, r);
+    GenerateFood(x_food, y_food, r);
 
-    VeTuong(1);
+    DrawBoundary(1);
 
     while (1) {
         if (_kbhit()) {
@@ -244,13 +244,13 @@ void CheDoCoDien(int& level, int& score, vector<HighScoreEntry>& highscore) {
         }
 
         // Xóa con rắn cũ
-        for (int i = 0; i < r.DoDai; i++) {
-            GoToXY(r.A[i].x, r.A[i].y);
+        for (int i = 0; i < r.length; i++) {
+            GoToXY(r.body[i].x, r.body[i].y);
             cout << " "; // Xóa phần thân cũ của con rắn
         }
 
         // Di chuyển con rắn và kiểm tra ăn mồi
-        r.DiChuyen(direction, x_moi, y_moi, r, 1, level, score);
+        r.DiChuyen(direction, x_food, y_food, r, 1, level, score);
 
         // Vẽ lại con rắn ở vị trí mới
         r.Ve();
@@ -260,30 +260,30 @@ void CheDoCoDien(int& level, int& score, vector<HighScoreEntry>& highscore) {
         cout << "Score: " << score;
 
         // Hiển thị điểm cao nhất
-        HienThiDiemCao(102, 2);
+        DisplayHighScore(102, 2);
 
         // Tốc độ di chuyển của con rắn
-        Sleep(r.TocDoRan);
+        Sleep(r.speed);
     }
 }
 
-void CheDoTuDo(int& level, int& score, vector<HighScoreEntry>& highscore) {
-    DemNguocBatDau();
-    XoaConTro();
+void FreeMode(int& level, int& score, vector<HighScoreEntry>& highscore) {
+    Countdown();
+    HideCursor();
     srand(time(0)); // Khởi tạo seed cho hàm rand
-    CONRAN r;
-    int direction = 0, x_moi = 0, y_moi = 0;
+    SNAKE r;
+    int direction = 0, x_food = 0, y_food = 0;
     char t;
 
     // Tạo tốc độ rắn theo từng mức độ
-    if (level == 1) r.TocDoRan = 200;
-    else if (level == 2) r.TocDoRan = 100;
-    else if (level == 3) r.TocDoRan = 50;
+    if (level == 1) r.speed = 200;
+    else if (level == 2) r.speed = 100;
+    else if (level == 3) r.speed = 50;
 
     // Tạo mồi ban đầu
-    TaoMoi(x_moi, y_moi, r);
+    GenerateFood(x_food, y_food, r);
 
-    VeTuong(2);
+    DrawBoundary(2);
 
     while (1) {
         if (_kbhit()) {
@@ -298,13 +298,13 @@ void CheDoTuDo(int& level, int& score, vector<HighScoreEntry>& highscore) {
         }
 
         // Xóa con rắn cũ
-        for (int i = 0; i < r.DoDai; i++) {
-            GoToXY(r.A[i].x, r.A[i].y);
+        for (int i = 0; i < r.length; i++) {
+            GoToXY(r.body[i].x, r.body[i].y);
             cout << " "; // Xóa phần thân cũ của con rắn
         }
 
         // Di chuyển con rắn và kiểm tra ăn mồi
-        r.DiChuyen(direction, x_moi, y_moi, r, 2, level, score);
+        r.DiChuyen(direction, x_food, y_food, r, 2, level, score);
 
         // Vẽ lại con rắn ở vị trí mới
         r.Ve();
@@ -314,17 +314,17 @@ void CheDoTuDo(int& level, int& score, vector<HighScoreEntry>& highscore) {
         cout << "Score: " << score;
 
         // Hiển thị điểm cao nhất
-        HienThiDiemCao(102, 2);
+        DisplayHighScore(102, 2);
 
         // Tốc độ di chuyển của con rắn
-        Sleep(r.TocDoRan);
+        Sleep(r.speed);
     }
 }
 
-void NhapNhay(CONRAN& r) {
+void Blink(SNAKE& r) {
     for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < r.DoDai; j++) {
-            GoToXY(r.A[j].x, r.A[j].y);
+        for (int j = 0; j < r.length; j++) {
+            GoToXY(r.body[j].x, r.body[j].y);
             cout << " ";
         }
         Sleep(500);
@@ -333,34 +333,34 @@ void NhapNhay(CONRAN& r) {
     }
 }
 
-void TaoMoi(int& x_moi, int& y_moi, CONRAN& r) {
+void GenerateFood(int& x_food, int& y_food, SNAKE& r) {
     do {
         // Tạo tọa độ ngẫu nhiên cho mồi
-        x_moi = rand() % 99 + 1;
-        y_moi = rand() % 24 + 1;
-    } while (CheckRanDeMoi(r, x_moi, y_moi));  // Kiểm tra xem mồi có trùng với vị trí con rắn không
-    GoToXY(x_moi, y_moi);
+        x_food = rand() % 99 + 1;
+        y_food = rand() % 24 + 1;
+    } while (CheckFoodCollision(r, x_food, y_food));  // Kiểm tra xem mồi có trùng với vị trí con rắn không
+    GoToXY(x_food, y_food);
     cout << "o";  // Hiển thị mồi
 }
 
-bool CheckRanDeMoi(CONRAN& r, int x_moi, int y_moi) {
+bool CheckFoodCollision(SNAKE& r, int x_food, int y_food) {
     // Kiểm tra mồi có trùng với các điểm trên thân rắn hay không
-    for (int i = 0; i < r.DoDai; i++) {
-        if (r.A[i].x == x_moi && r.A[i].y == y_moi) {
+    for (int i = 0; i < r.length; i++) {
+        if (r.body[i].x == x_food && r.body[i].y == y_food) {
             return true;
         }
     }
     return false;
 }
 
-void XoaConTro() {
+void HideCursor() {
     CONSOLE_CURSOR_INFO Info;
     Info.bVisible = FALSE;
     Info.dwSize = 20;
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &Info);
 }
 
-void VeTuong(int gameMode) {
+void DrawBoundary(int gameMode) {
     if (gameMode == 1) {
         for (int i = 0; i <= 100; i++) {
             GoToXY(i, 0);
@@ -403,7 +403,7 @@ void GoToXY(int column, int line) {
 
 
 // Hàm đọc danh sách top 10 điểm số
-void DocDiemCao(vector<HighScoreEntry>& highScores) {
+void ReadHighScores(vector<HighScoreEntry>& highScores) {
     ifstream infile("highscore.txt");
     if (infile) {
         HighScoreEntry entry;
@@ -415,7 +415,7 @@ void DocDiemCao(vector<HighScoreEntry>& highScores) {
 }
 
 // Hàm ghi danh sách top 10 điểm số
-void VietDiemCao(vector<HighScoreEntry>& highScores) {
+void WriteHighScores(vector<HighScoreEntry>& highScores) {
     ofstream outfile("highscore.txt");
     for (const auto& entry : highScores) {
         outfile << entry.name << " " << entry.score << endl;
@@ -424,9 +424,9 @@ void VietDiemCao(vector<HighScoreEntry>& highScores) {
 }
 
 // Hàm thêm điểm mới vào bảng xếp hạng
-void ThemDiemCao(int score) {
+void AddHighScore(int score) {
     vector<HighScoreEntry> highScores;
-    DocDiemCao(highScores);
+    ReadHighScores(highScores);
 
     // Hiển thị thông báo nếu điểm mới lọt top 10
     if (highScores.size() < 10 || score > highScores.back().score) {
@@ -453,14 +453,14 @@ void ThemDiemCao(int score) {
         }
 
         // Ghi lại danh sách mới vào file
-        VietDiemCao(highScores);
+        WriteHighScores(highScores);
     }
 }
 
-void HienThiDiemCao(int x, int y) {
+void DisplayHighScore(int x, int y) {
     // Đọc danh sách điểm cao từ file
     vector<HighScoreEntry> highScores;
-    DocDiemCao(highScores);
+    ReadHighScores(highScores);
 
     // Lấy điểm cao nhất và tên tương ứng
     int highscore = (highScores.empty()) ? 0 : highScores.front().score;
@@ -474,15 +474,15 @@ void HienThiDiemCao(int x, int y) {
 }
 
 
-void AmThanhAnMoi() {
+void PlayFoodSound() {
     PlaySound(TEXT("eatFood.wav"), NULL, SND_FILENAME | SND_ASYNC);
 }
 
-void AmThanhThuaCuoc() {
+void PlayGameOverSound() {
     PlaySound(TEXT("endGame.wav"), NULL, SND_FILENAME | SND_ASYNC);
 }
 
-void DemNguocBatDau() {    // Hàm hiển thị đếm ngược
+void Countdown() {    // Hàm hiển thị đếm ngược
     for (int i = 3; i > 0; i--) {
         system("cls");
         GoToXY(50, 12);  // Đặt vị trí hiển thị ở giữa màn hình
